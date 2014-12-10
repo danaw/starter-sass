0666a7dbac0a11e4accf3f6636037b577990a43c
danaw <dana.werpny@gmail.com>
2014-12-10 17:55:42 -0500

    Both navigations include hamburger menu

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -1,8 +1,8 @@
 ( function( window, $, undefined ) {
 	'use strict';
 
-	$( '.nav-secondary' ).before( '<button class="menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to menus
-	$( '.nav-secondary .sub-menu' ).before( '<button class="sub-menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to sub menus
+	$( 'nav' ).before( '<button class="menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to menus
+	$( 'nav .sub-menu' ).before( '<button class="sub-menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to sub menus
 
 	// Show/hide the navigation
 	$( '.menu-toggle, .sub-menu-toggle' ).on( 'click', function() {
@@ -12,7 +12,7 @@
 		});
 
 		$this.toggleClass( 'activated' );
-		$this.next( '.nav-secondary, .sub-menu' ).slideToggle( 'fast' );
+		$this.next( 'nav, .sub-menu' ).slideToggle( 'fast' );
 
 	});
 
